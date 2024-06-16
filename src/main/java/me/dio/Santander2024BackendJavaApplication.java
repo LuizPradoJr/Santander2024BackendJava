0f86b4a6f0d1705df50e91cc3e7aa0e93066f446
Luiz Prado <luizpradojr@gmail.com>
2024-06-15 22:18:16 -0300

    Santander2024BackendJava

---
+++ src/main/java/me/dio/Santander2024BackendJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2024BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2024BackendJavaApplication.class, args);
+	}
+
+}
